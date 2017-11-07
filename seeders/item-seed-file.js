ded030b70a0229777e5c8db767a967f8cd560719
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-11-06 19:28:23 -1000

    included includes in requests

--- seeders/item-seed-file.js
+++ seeders/item-seed-file.js
@@ -8,19 +8,25 @@ module.exports = {
         name: 'kevin',
         image: 'https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Fish/H-P/pufferfish-closeup.ngsversion.1427141760081.adapt.1900.1.jpg',
         body: 'This is a fish.',
-        price: 34
+        price: 34,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         name: 'chris',
         image: 'http://www.ikea.com/PIAimages/0406160_PE569308_S5.JPG',
         body: 'This is a chair.',
-        price: 14
+        price: 14,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         name: 'virgi',
         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhjiVcw4xjCpHrW5hI3fU1tCxb0Lu6azd5sUkrlwcncBoD-lNl',
         body: 'This is a car.',
-        price: 134
+        price: 134,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }], {});
 
   },
--- server/routes/Items.js
+++ server/routes/Items.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const items = require('./items');
-const db = require('../models');
+const db = require('../../models');
 const Items = db.item;
 const Category = db.category;
 
-// router.post('/new', (req, res) => {
-//   let data = req.body;
-//   return Items.create({
-//     name: data.name,
 
-//   })
-// })
+router.post('/new', (req, res) => {
+  let data = req.body;
+  return Items.create({
+    name: data.name,
+    image: data.image,
+    body: data.body,
+    price: data.price,
+    category_id: data.category_id,
+    condition_id: data.condition_id,
+    user_id: req.user.id,
+    status_id: data.status_id
+  });
+});
 
 router.get('/all', (req, res) => {
-  return Items.findAll({include:[]})
+  return Items.findAll({include:[
+    {model:Category, as: 'Category'},
+    {model: Condition, as: 'Condition'},
+    {model: User, as: 'User'},
+    {model: Status, as: 'Status'}
+    ]})
   .then((items) => {
     return res.json(items)
   })
