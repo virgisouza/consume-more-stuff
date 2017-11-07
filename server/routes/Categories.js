bccaa28634aefb911509d414f2825e7a234e369f
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-11-06 18:52:58 -1000

    started on item requests

--- server/routes/Categories.js
+++ server/routes/Categories.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const categories = require('./categories');
+// const db = require('../models');
+// const Categories = db.category;
+
 
 module.exports = router;
--- server/routes/Items.js
+++ server/routes/Items.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const items = require('./items');
-// const db = require('../models');
-// const Items = db.item;
+const db = require('../models');
+const Items = db.item;
+const Category = db.category;
 
-// router.get('/all', (req, res) => {
-//   return Items.findAll({include:[{model:}]})
-//   .then
+// router.post('/new', (req, res) => {
+//   let data = req.body;
+//   return Items.create({
+//     name: data.name,
+
+//   })
 // })
+
+router.get('/all', (req, res) => {
+  return Items.findAll({include:[]})
+  .then((items) => {
+    return res.json(items)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+router.get('/:category_id', (req, res) => {
+  return Items.findAll({include:[],
+    where: {
+      category_id: req.params.category_id
+    }
+  })
+  .then((items) => {
+    return res.json(items)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+router.get('/:category_id/item/:id', (req, res) => {
+  return Items.findOne({include:[],
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((item) => {
+    return res.json(item);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
 
 module.exports = router;
