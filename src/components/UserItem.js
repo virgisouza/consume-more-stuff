31b3e3ed7c8b05bc8e11676c4053d40676c43d65
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-11-07 11:19:28 -1000

    setup front end routes for login register

---
+++ src/components/UserItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserItem = ({username, password, email}) => {
+
+  return (
+    <div className="new-user">
+      <div>Username : {username}</div>
+      <div>Password : {password}</div>
+      <div>Email : {email}</div>
+    </div>
+  )
+}
+
+export default UserItem;
