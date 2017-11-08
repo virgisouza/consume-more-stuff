a151b40c7286d82c9a49b3940e8c67ba6d84e497
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-11-07 16:32:28 -1000

    created edit item form and auth user item view

--- src/components/item.js
+++ src/components/item.js
@@ -1,16 +1,36 @@
-/*import React from 'react';
-import '../containers/App/App.css';
+// Unauthorized users item view
+import React from 'react';
 
-const Item = ({ description, price, make, model, condition, category, dimensions, datePublished, lastUpdated, details }) => {
+const Item = ({image, body, price, condition, category, createdAt}) => {
 
-  return (
-    <div className="Item">
-      <div>Description : { description }</div>
-      <div>Price : { price }</div>
-    </div>
+  return  (
+    <ul className='item'>
+      <div>Image : {image}</div>
+      <div>Description : {body}</div>
+      <div>Price : {price}</div>
+      <div>Condition : {condition}</div>
+      <div>Category : {category}</div>
+      <div>When Item was posted: {createdAt}</div>
+    </ul>
   );
-  
+
 }
 
+export default Item;
 
-export default Item;*/
+// Create an attractive Detail view that fits with the rest of the site that presents the following information to the user:
+
+// - description
+// - image
+// - price, if listed
+// - condition
+// - category
+// - manufacturer
+// - model
+// - dimensions
+// - notes
+// - when the item was posted
+// - when the item was last updated
+
+
+// Unauthorized users will only be able to see "published" items.
---
+++ src/containers/AuthUserEditItem/index.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import Item from '../../components/Item';
+import { connect } from 'react-redux';
+//import load one item function
+
+class AuthUserEditItem extends Components {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      name: '',
+      image: '',
+      body: '',
+      price: '',
+      category_id: '',
+      condition_id: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    let editItem = {
+      name: this.state.name,
+      image: this.state.image,
+      body: this.state.body,
+      price: this.state.price,
+      category_id: this.state.category_id || 1,
+      condition_id: this.state.condition_id || 1
+    }
+
+    this.props.editItem(editItem);
+
+    this.setState({
+      name: '',
+      image: '',
+      body: '',
+      price: '',
+      category_id: '',
+      condition_id: ''
+    })
+  }
+
+  handleChangeName(event){
+    event.preventDefault();
+    this.setState({name: event.target.value});
+  }
+
+  handleChangeImage(event){
+    event.preventDefault();
+    this.setState({image: event.target.value});
+  }
+
+  handleChangeBody(event){
+    event.preventDefault();
+    this.setState({body: event.target.value});
+  }
+
+  handleChangePrice(event){
+    event.preventDefault();
+    this.setState({price: event.target.value});
+  }
+
+  handleChangeCategory(event){
+    event.preventDefault();
+    this.setState({category_id: event.target.value});
+  }
+
+  handleChangeCondition(event){
+    event.preventDefault();
+    this.setState({condition_id: event.target.value});
+  }
+
+  render(){
+
+    return (
+      <div className='EditItem'>
+        {
+          this.props.items.filter(item => {
+            //kevin help here
+            return
+          })
+          .then(props => {
+
+            return (
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <input
+                  type='text'
+                  placeholder='Name'
+                  value={this.state.name}
+                  onChange={this.handleChangeName.bind(this)}
+                />
+
+                <input
+                  type='text'
+                  placeholder='Image Url'
+                  value={this.state.image}
+                  onChange={this.handleChangeImage.bind(this)}
+                />
+
+                <input
+                  type='text'
+                  placeholder='Body'
+                  value={this.state.body}
+                  onChange={this.handleChangeBody.bind(this)}
+                />
+
+                <input
+                  type='text'
+                  placeholder='Price'
+                  value={this.state.price}
+                  onChange={this.handleChangePrice.bind(this)}
+                />
+
+                <Select
+                  list={this.props.categories}
+                  label='Category: '
+                  type='name'
+                  handler={this.handleChangeCategory.bind(this)}
+                />
+
+                <Select
+                  list={this.props.conditions}
+                  label='Condition : '
+                  type='type'
+                  handler={this.handleChangeCondition.bind(this)}
+                />
+                <button type='submit'>Submit</button>
+              </form>
+            )
+          })
+
+
+        }
+      </div>
+
+
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+const ConnectedAuthUserEditItem = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AuthUserEditItem)
+
+export default ConnectedAuthUserEditItem;
