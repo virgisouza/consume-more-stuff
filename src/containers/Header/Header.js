5d554039b20f476d2f0b8e830219969b69fb4f1a
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-11-09 18:44:55 -1000

    login form is hidden on logout

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -27,6 +27,9 @@ class Header extends Component {
   handleLogout(event) {
     event.preventDefault();
     this.props.logoutUser();
+    this.setState({
+      showLogin: false
+    })
   }
 
   showLogin() {
