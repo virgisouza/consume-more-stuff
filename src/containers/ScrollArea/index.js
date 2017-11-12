4df70d00d37942ae4e5903ce673772a44680ae42
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-11-11 18:45:03 -1000

    get clean

---
+++ src/containers/ScrollArea/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../App/App.css';
+
+class ScrollArea extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return(
+      <ScrollArea
+        speed={0.8}
+        className="area"
+        contentClassName="content"
+        horizontal={false}
+      >
+      <div>Some long content.</div>
+      </ScrollArea>
+    )
+  }
+}
+
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.itemList
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     loadUserItems: (id) => {
+//       dispatch(loadUserItems(id));
+//     }
+//   }
+// }
+
+const ConnectedScrollArea = connect(
+  null,
+  null
+)(ScrollArea);
+
+export default ConnectedScrollArea;
