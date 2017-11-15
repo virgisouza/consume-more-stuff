468849d395e2f0f86459600fedd9a253f4e45a63
Chris Min <chris.min216@gmail.com>
2017-11-14 20:02:50 -1000

    Login Form done..experimenting with AWS

--- src/containers/Forms/LoginForm.js
+++ src/containers/Forms/LoginForm.js
@@ -4,6 +4,9 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+/*ACTIONS*/
 import { loginUser } from '../../actions/users';
 
 import './LoginForm.css';
@@ -12,81 +15,61 @@ class LoginForm extends Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      handle : '',
+      password : ''
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event){
-    event.preventDefault();
-    let user = {
-      username: this.state.username,
-      password: this.state.password
-    }
-    this.props.loginUser(user);
-
-    this.setState({
-      username: '',
-      password: ''
-    })
+  validateForm() {
+    return this.state.handle.length > 0 && this.state.password.length > 0;
   }
 
-  handleChangeUsername(event){
-    event.preventDefault();
+  handleChange = event => {
     this.setState({
-      username: event.target.value
-    })
+      [event.target.id] : event.target.value
+    });
   }
 
-  handleChangePassword(event){
+  handleSubmit = event => {
     event.preventDefault();
-    this.setState({
-      password: event.target.value
-    })
   }
 
   render() {
     console.log('Login Form rendered');
     return (
-
       <div className="Login-form">
-
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <div className="Login-box">
-            <span>Log In</span>
-          </div>
-          <div className="Login-handle">
-            <span>Email/Handle</span>
-          </div>
-            
-           <div>
-            <input
-              type='text'
-              placeholder=''
-              onFocus={(e) => e.target.placeholder=""}
-              onBlur={(e) => e.target.placeholder=""}
-              onChange={this.handleChangeUsername.bind(this)}
+          <ControlLabel className='Login-box'>Log In</ControlLabel>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email/Handle</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.handle}
+              onChange={this.handleChange}
             />
-           </div>
-
-          <div className="Login-pw">
-            <span>Password</span>
-          </div>
-
-            <div>
-              <input
-                type='text'
-                placeholder=''
-                onFocus={(e) => e.target.placeholder=""}
-                onBlur={(e) => e.target.placeholder=""}
-                onChange={this.handleChangePassword.bind(this)}
-              />
-            </div>
-            
-            <div className="Login-bottom">
-              <button type='submit'>Log In</button>
-              <a>Forgot Password?</a>
-            </div>
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              autoFocus
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
         </form>
-
       </div>
 
     );
